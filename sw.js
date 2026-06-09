fc690d1d9f25925fbbffec09bd48769370049d92
shunpedesu <shunuehara888@gmail.com>
2026-06-09 17:25:42 +0900

    PWA対応：ホーム画面追加バナー・manifest・Service Worker追加

---
+++ sw.js
@@ -0,0 +1,10 @@
+const CACHE = 'sekibo-v1';
+self.addEventListener('install', e => {
+  self.skipWaiting();
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(clients.claim());
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+});
